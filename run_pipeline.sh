d0273ea97424df0cca530cdf349b2e66693981ab
Cemsel Bafligil Suomi <86775937+cemselb@users.noreply.github.com>
2026-06-05 11:24:52 +0200

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 bash bin/download_data.sh
 
 nextflow run nf-core/methylseq \
     -r 2.6.0 \
     -profile standard \
-    -c nextflow.config
+    -c nextflow.config \
+    --input data/samplesheet.csv \
+    --outdir results \
+    --fasta data/reference/hg38_chr21.fa \
+    --emseq
 
 Rscript bin/analyse_methylation.R
